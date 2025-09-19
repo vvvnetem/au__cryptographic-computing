f84a40da25989bf478fde96640987fc49e0fb8dc
fanniferenczi <fanniferenczi24@gmail.com>
2025-09-19 14:22:04 +0200

    bedoza-passive

---
+++ BeDOZa-passive/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*Dealer.Init();
+        Alice.Init(x,Dealer.RandA());
+        Bob.Init(y,Dealer.RandB());
+        while(Alice.hasOutput()==false)
+        {
+            Bob.Receive(Alice.Send());
+            Alice.Receive(Bob.Send());
+        }
+        z = Alice.Output();
+        (Note that the loop is not strictly necessary, since you are implementing the protocol for a specific
+        function so you can predict in advance how many rounds there will be).*/
+    }
+}
