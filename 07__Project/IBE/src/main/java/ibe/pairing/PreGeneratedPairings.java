ef4700899e0081ec3392826eaddd7198c40d9c2d
Miklos Vadasz <153357729+vvvnetem@users.noreply.github.com>
2025-11-23 00:32:13 +0100

    Type F pairing parameters for BLS12-381
    Added Type F pairing parameters for BLS12-381 to support quick testing.

--- 07__Project/IBE/src/main/java/ibe/pairing/PreGeneratedPairings.java
+++ 07__Project/IBE/src/main/java/ibe/pairing/PreGeneratedPairings.java
@@ -11,7 +11,7 @@ import java.io.ByteArrayInputStream;
  * Pre-generated pairing parameters for quick testing
  */
 public class PreGeneratedPairings {
-
+   
     /**
      * Type A pairing parameters (160-bit r, 512-bit q)
      * Pre-generated for quick initialization
@@ -26,6 +26,33 @@ public class PreGeneratedPairings {
                     "sign1 1\n" +
                     "sign0 1\n";
 
+    /**
+     * Type F pairing parameters (in hexadecimal) for BLS12-381.
+     * For quick test purpose. jPBC is not suited for BLS curves.
+     * Apache Milagro (AMCL) has hash_to_curve() according to RFC9830
+     -------------------------------------------------------------
+         "type f\n" +
+            "q 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab\n" +
+            "r 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001\n" +
+            "h 0x1\n" +
+            "b 0x4\n" +
+            "nqr 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaaa\n" +
+            "c0 0x1\n" +
+            "c1  0x1\n" +
+            "twist_type m\n" +
+            "bt_c0 0x4\n" +
+            "bt_c1 0x4\n" +
+            "g1_x 0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb\n" +
+            "g1_y 0x08b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1\n" +
+            "g2_x1 0x024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8\n" +
+            "g2_x0 0x13e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e\n" +
+            "g2_y1 0x0ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801\n" +
+            "g2_y0 0x0606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be\n" +
+            "x 0xd201000000010000\n" +
+            "sign -1\n"
+     */
+
+    
     /**
      * Create pairing from pre-generated Type A parameters
      */
